1807a0ec077f5672834f7896df8cc6af464c228c
lingye1005 <caoxiaohong1005@163.com>
2017-09-05 20:26:52 +0800

    测试up

--- src/jianzhioffer/GetUglyNumber.java
+++ src/jianzhioffer/GetUglyNumber.java
@@ -1,8 +1,5 @@
 package jianzhioffer;
 
-import java.util.ArrayList;
-import java.util.Collections;
-
 /**
  * Created by caoxiaohong on 17/9/3.
  * 把只包含因子2、3和5的数称作丑数（Ugly Number）。例如6、8都是丑数，但14不是，因为它包含因子7。 习惯上我们把1当做是第一个丑数。求按从小到大的顺序的第N个丑数。
@@ -14,47 +11,40 @@ import java.util.Collections;
  */
 public class GetUglyNumber {
     public int GetUglyNumber_Solution(int index) {
-        if(index==1)
-            return 1;
-        else if(index==2)
-            return 2;
-        else if(index==3)
-            return 3;
-        else if(index==4)
-            return 5;
-
-        ArrayList<Integer> values=new ArrayList<Integer>();
-        //values.add(1);
-        values.add(2);
-        values.add(3);
-        values.add(5);
-        while (values.size()<index-1) {
-            //*2
-            for (int i = 0; i < values.size(); i++) {
-                if (!values.contains(values.get(i) * 2)) {
-                    values.add(values.get(i) * 2);
-                    if (values.size() == index - 1)
-                        break;
-                }
-            }
-            //*3
-            for(int i=values.size()/2;i<values.size();i++){
-                if (!values.contains(values.get(i) * 3)) {
-                    values.add(values.get(i) * 3);
-                    if (values.size() == index - 1)
-                        break;
-                }
-            }
-            //*5
-
+        if(index<0)
+            return 0;
+        int[] values=new int[index];
+        values[0]=1;
+        int count=1;
+        int idx2=0,idx3=0,idx5=0;
+        while (count<index){
+            int minNum=min(values[idx2]*2,values[idx3]*3,values[idx5]*5);
+            values[count]=minNum;
+            while (values[idx2]*2<=values[count])
+                idx2++;
+            while (values[idx3]*3<=values[count])
+                idx3++;
+            while (values[idx5]*5<=values[count])
+                idx5++;
+            count++;
         }
-        values.add(1);
-        Collections.sort(values);
-        return values.get(values.size()-1);
+        return values[index-1];
+    }
+
+    /**
+     * 求3个数中最小的数字
+     * @param a1
+     * @param a2
+     * @param a3
+     * @return
+     */
+    int min(int a1,int a2,int a3){
+        int tmp=Math.min(a1,a2);
+        return Math.min(tmp,a3);
     }
 
     public static void main(String[] args) {
         GetUglyNumber t=new GetUglyNumber();
-        System.out.println(t.GetUglyNumber_Solution(10));
+        System.out.println(t.GetUglyNumber_Solution(1500));
     }
 }
